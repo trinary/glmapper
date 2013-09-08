500ff5c16bd3ecad4a25fa2179ac730be90caa92
Erik Cunningham <djtrinary@gmail.com>
2013-09-08 16:16:44 -0600

    scene

--- example.js
+++ example.js
@@ -4,4 +4,4 @@ var fs = document.getElementById('fs');
 var scene = glmapper.scene(document.getElementById("mapper")).addVertexShader(vs);
 scene.addFragmentShader(fs)
 scene.compileShaders().programShaders().link();
-scene(1);
+scene();
